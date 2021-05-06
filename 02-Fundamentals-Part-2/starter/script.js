10324602bc3ee966e5fb2c2ab5b2130c6d82a08e
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2021-05-06 10:36:46 +0530

    Learned about arrays: how to declare them, how to access array elements, how to reassign a new element into an array, how to use arrays in function arguments

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -89,25 +89,69 @@
 // console.log(fruitProcessor(2, 3));
 
 
-const calcAge = function(birthYear) {
+// const calcAge = function(birthYear) {
+//     return 2037 - birthYear;
+// }
+
+// const yearsUntilRetirement = function(birthYear, firstName) {
+//     const age = calcAge(birthYear)
+//     const retirement = 65 - age;
+
+//     if (retirement > 0) {
+//         console.log(`${firstName} retires in ${retirement} years`)
+//         return retirement
+//     } else {
+//         console.log(`${firstName} has already retired 🎉`);
+//         return -1;
+//     }
+
+
+//     // return `${firstName} retires in ${retirement} years`;
+// }
+
+// console.log(yearsUntilRetirement(1991, "Jonas"));
+// console.log(yearsUntilRetirement(1950, "Mike"));
+
+
+
+// Arrays
+const friends = ["Michael", "Steven", "Peter"];
+console.log(friends);
+
+const y = new Array(1991, 1984, 2008, 2020);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length -1 ]);
+
+friends[2] = "Jay";
+console.log(friends);
+
+
+
+
+const city = "Lisbon";
+const jonas = ["Jonas", "Khatri", 2037 - 1991, city, "teacher", friends];
+
+console.log(jonas);
+
+
+
+// Exercise
+const calcAge = function (birthYear) {
     return 2037 - birthYear;
 }
 
-const yearsUntilRetirement = function(birthYear, firstName) {
-    const age = calcAge(birthYear)
-    const retirement = 65 - age;
-
-    if (retirement > 0) {
-        console.log(`${firstName} retires in ${retirement} years`)
-        return retirement
-    } else {
-        console.log(`${firstName} has already retired 🎉`);
-        return -1;
-    }
+const years = [1990, 1967, 2002, 2010, 2018];
 
 
-    // return `${firstName} retires in ${retirement} years`;
-}
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
 
-console.log(yearsUntilRetirement(1991, "Jonas"));
-console.log(yearsUntilRetirement(1950, "Mike"));
+console.log(age1, age2, age3);
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
+console.log(ages);
