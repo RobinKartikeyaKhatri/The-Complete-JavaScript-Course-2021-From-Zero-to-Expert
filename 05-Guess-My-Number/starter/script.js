641ff9c5f570e7bfb60f7d542650977fbe5e1823
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2021-05-20 09:35:39 +0530

    Learned how to select and manipulate elements using DOM - how to get and set input elements value using '.value' property instead of '.textContent' property

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,4 +1,17 @@
 'use strict';
 
-const message = document.querySelector(".message");
-console.log(message.textContent);
+const message = document.querySelector('.message');
+
+message.textContent = '🎉 Correct Number';
+
+const number = document.querySelector(".number");
+number.textContent = 13;
+
+const score = document.querySelector(".score");
+score.textContent = 10;
+
+const guess = document.querySelector(".guess");
+console.log(guess.value);
+
+guess.value = 23;
+console.log(guess.value);
