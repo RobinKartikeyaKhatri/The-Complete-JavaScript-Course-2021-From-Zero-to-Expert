044a0022ce2461085d37cf7e6cff8f80199228d2
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2021-05-28 16:07:15 +0530

    Learned how to add and remove classes from HTML elements using add() and remove() methods available on classList property

--- 06-Modal-Window/starter/script.js
+++ 06-Modal-Window/starter/script.js
@@ -5,10 +5,20 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-console.log(btnsOpenModal);
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const openModal = () => {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+};
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener('click', function () {
-    console.log(`${btnsOpenModal[i].textContent}`);
-  });
+  btnsOpenModal[i].addEventListener('click', openModal);
 }
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
