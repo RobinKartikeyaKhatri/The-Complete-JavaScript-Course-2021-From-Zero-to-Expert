3e6f4a410e772a5aaafd4a6b4593183346f11c78
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2021-05-30 10:02:59 +0530

    Learned about 'event' object inside event handler functions, also learned how to handle keyboard events in JavaScript

--- 06-Modal-Window/starter/script.js
+++ 06-Modal-Window/starter/script.js
@@ -11,8 +11,8 @@ const closeModal = () => {
 };
 
 const openModal = () => {
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
 };
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
@@ -22,3 +22,7 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 btnCloseModal.addEventListener('click', closeModal);
 
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) closeModal();
+});
