ba40a8cd02aa7e5947b39e976096cf5b065341be
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2021-07-07 15:40:57 +0530

    Learned how scoping works in JavaScript

---
+++ 08-Behind-the-Scenes/starter/script.js
@@ -0,0 +1,42 @@
+'use strict';
+
+// Defined in Global Scope - That's why it is in Top-Level code
+// Also this function creates its own scope - and that scope is equivalent to the variable environment to its execution context
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+        var millenial = true;
+        const firstName = "Steven";
+        const str = `Oh, and you're a millenial, ${firstName}`;
+        console.log(str);
+
+        function add(a, b) {
+            return a + b;
+        }
+
+        output = "NEW OUTPUT";
+
+    }
+
+    // console.log(str);
+    console.log(millenial);
+    // console.log(add(2, 3));
+    console.log(output);
+  }
+
+  printAge();
+
+  return age;
+}
+
+// Gloal Variable
+const firstName = 'Jonas';
+
+calcAge(1991);
+// console.log(age);
+// printAge();
